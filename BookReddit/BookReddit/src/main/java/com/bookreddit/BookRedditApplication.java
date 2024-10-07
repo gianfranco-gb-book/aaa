2db512600c8275446a506c8eb8c69eef5f87622c
gianfranco-gb-book <megagian12@gmail.com>
2024-10-07 18:18:08 +0200

    Add files via upload

---
+++ BookReddit/BookReddit/src/main/java/com/bookreddit/BookRedditApplication.java
@@ -0,0 +1,13 @@
+package com.bookreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRedditApplication.class, args);
+	}
+
+}
